1b9455ac23cb5cfc7e00c19c4cba4458c01f258e
Akum6x <dfrale@mail.ru>
2025-10-02 16:42:02 +0300

    Update White.cs

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab2
+namespace Lab2
 {
     public class White
     {
@@ -8,7 +8,11 @@
             int answer = 0;
 
             // code here
-
+            if (n <= 0) return 0;
+            for(int i = 1; i <= n; i++)
+            {
+                answer += 3 * i - 1;
+            }
             // end
 
             return answer;
@@ -18,7 +22,13 @@
             double answer = 0;
 
             // code here
-
+            if (n <= 0) return 0;
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1.0 / i;
+            }
+            answer = sum;
             // end
 
             return answer;
@@ -28,7 +38,12 @@
             long answer = 0;
 
             // code here
-
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            answer = factorial;
             // end
 
             return answer;
@@ -38,7 +53,19 @@
             long answer = 0;
 
             // code here
+            if (b == 0) return 1;
 
+            int absB = Math.Abs(b);
+
+            for (int i = 0; i < absB; i++)
+            {
+                answer *= a;
+            }
+
+            if (b < 0)
+            {
+                answer = 1 / answer;
+            }
             // end
 
             return answer;
@@ -48,6 +75,23 @@
             int answer = 0;
 
             // code here
+            if (L < 1) return 1;
+
+            int n = 1;
+            long product = 1;
+
+            while (product <= L)
+            {
+                product *= n;
+
+                if (product > L)
+                {
+                    return n;
+                }
+
+                n += 3;
+            }
+            answer = n;
 
             // end
 
@@ -58,21 +102,30 @@
             double answer = 0;
 
             // code here
+            double sum = 0;
+            double term = 1;
 
-            // end
-
+            while (term >= 1e-4)
+            {
+                sum += term;
+                term *= x * x;
+                answer++;
+                // end
+            }
             return answer;
         }
 
         public int Task7(int n)
         {
             int answer = 0;
-
+            int sum = 0;
             // code here
-
-            // end
-
-            return answer;
+            if (sum < n == true)
+            {
+                answer++;
+                sum += answer;
+            }
+                return answer;
         }
         public int Task8(double L, double v)
         {
