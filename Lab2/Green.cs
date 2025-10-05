b9c24e07cd930cc9bdd2d4189984d1792d1bc707
Akum6x <dfrale@mail.ru>
2025-10-05 14:48:45 +0300

    Update2 Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lab2
 {
@@ -11,7 +12,10 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            for (int i = 2; i <= n; i += 2)
+            {
+                answer += (double)i / (i + 1);
+            }
             // end
 
             return answer;
@@ -21,7 +25,12 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double term = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                answer += term;
+                term /= x;
+            }
             // end
 
             return answer;
@@ -31,7 +40,12 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            long factorial = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i > 0) factorial *= i;
+                answer += factorial;
+            }
             // end
 
             return answer;
@@ -41,7 +55,15 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double term = 1;
+            int n = 1;
+            double epsilon = 1e-4;
+             while (Math.Abs(term) >= epsilon)
+            {
+                term = Math.Sin(n * Math.Pow(x, n));
+                answer += term;
+                n++;
+            }
             // end
 
             return answer;
@@ -51,7 +73,16 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int n = 2;
+            double prev = 1.0 / x;
+            double current = 1.0 / (x * x);
+            while (Math.Abs(current - prev) >= 1e-4)
+            {
+                n++;
+                prev = current;
+                current /= x;
+            }
+            answer = n;
             // end
 
             return answer;
@@ -61,7 +92,13 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            int elem = 1;
 
+            for (int i = 0; elem < limit; i++)
+            {
+                elem *= 2;
+                answer += elem;
+            }
             // end
 
             return answer;
@@ -72,7 +109,12 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double atomDiameter = 1e-10;
+            while (L > atomDiameter)
+            {
+                L /= 2;
+                answer++;
+            }
             // end
 
             return answer;
@@ -83,7 +125,17 @@ namespace Lab2
             double SY = 0;
 
             // code here
-
+            for (double x = a; x <= b; x+=h)
+            {
+                double k = x;
+                SS += x;
+                SY += Math.Atan(x);
+                for (double i = 0; Math.Abs(k) >= 0.0001; i++)
+                {
+                    k *= (-1) * x * x * (2 * i + 1) / (2 * i + 3);
+                    SS += k;
+                }
+            }
             // end
 
             return (SS, SY);
